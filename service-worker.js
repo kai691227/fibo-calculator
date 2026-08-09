1f6827bbb6f63959451df57ecde06474ea43e13c
kai691227 <kai095919@gmail.com>
2026-08-09 10:17:09 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,9 @@
-
-const CACHE_NAME = "fibo-navigation-v12";
+const CACHE_NAME = "fibo-navigation-v121";
 const ASSETS = [
-  "./",
   "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
+  "./style.css?v=121",
+  "./app.js?v=121",
+  "./manifest.json?v=121",
   "./icon-192.png",
   "./icon-512.png"
 ];
@@ -16,16 +14,19 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
-  );
+  event.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+  ));
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html")))
   );
 });
